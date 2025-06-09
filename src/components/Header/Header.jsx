599fefaae2b961583add6cf0d24316b3e7bd777d
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-09 20:54:55 +0530

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import quizLogo from "/quiz.png";
+import "./Header.css";
+function Header() {
+    return (
+        <header>
+            <img src={quizLogo} alt="AutoQuiz logo" />
+            <h1>Auto Quiz</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/TopicForm/TopicForm.jsx
@@ -0,0 +1,9 @@
+function TopicForm() {
+    return (
+        <form action="">
+            <input type="text" name="" id="" />
+        </form>
+    )
+}
+
+export default TopicForm;
