ebcfa1a00210f47427892ea9aed4231b3205bda6
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-11 20:02:50 +0530

    update submit handler with the declarative side of React

--- src/components/TopicForm/TopicForm.jsx
+++ src/components/TopicForm/TopicForm.jsx
@@ -1,17 +1,16 @@
 import './TopicForm.css';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'; // For free Solid style
 
 function TopicForm() {
     const [topics, setTopics] = useState([]);
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
+    function handleSubmit(formData) {
         const newTopic = formData.get("topic").trim();
         if(!newTopic || topics.find(topic => topic == newTopic))
             return;
         console.log(topics);
         setTopics(prevTopics => [...prevTopics,newTopic]);
-        event.target.reset();
     }
     function removeTopic(topic) {
         setTopics(topics.filter(currTopic => currTopic !== topic));
@@ -22,13 +21,15 @@ function TopicForm() {
     }
     const topicList = topics.map(topic => <li>{topic}<button onClick={() => removeTopic(topic)} type="button" aria-label='remove topic'>x</button></li>);
     return (
-        <form onSubmit={handleSubmit} >
+        <form action={handleSubmit} >
             <p>Add topics to generate a quiz below</p>
             <main>
                 <input type="text" name="topic" placeholder="e.g React hooks" />
                 <button type="submit" >+ Add topic</button>
             </main>
-            <button onClick={removeAllTopics}>clear</button>
+            <button onClick={removeAllTopics}>
+                <FontAwesomeIcon icon={faDeleteLeft}/> clear
+            </button>
             {topics.length !==0 && <ul>{topicList}</ul> }
         </form>
     )
