fb7bf62ac5caee2c2903a82544bb6f52e0833ec2
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-12 22:28:53 +0530

    add styles to upadted form

--- src/components/TopicForm/TopicForm.jsx
+++ src/components/TopicForm/TopicForm.jsx
@@ -1,7 +1,7 @@
 import './TopicForm.css';
 import { useState,useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'; // For free Solid style
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
 
 function TopicForm() {
     const [topics, setTopics] = useState([]);
@@ -26,13 +26,14 @@ function TopicForm() {
     }
     const topicList = topics.map(topic => <li>{topic}<button onClick={() => removeTopic(topic)} type="button" aria-label='remove topic'>x</button></li>);
     return (
-        <>
-            <form action={addTopic} >
-                <p>Add topics to generate a quiz below</p>
+        <main id="whole-main">
+            <form action={addTopic} id="topicForm">
+                <h3>Add topics to generate a quiz below</h3>
                 <main>
                     <input type="text" name="topic" placeholder="e.g React hooks" ref={topicRef}/>
                     <button >+ Add topic</button>
                 </main>
+                {topics.length !==0 && <><h3>Topics Selected:</h3><ul>{topicList}</ul></> }
                 {
                     topics.length >0 ?
                     <button onClick={removeAllTopics}>
@@ -40,25 +41,21 @@ function TopicForm() {
                     </button>:
                     <p>Enter a topic to continue</p>
                 }
-                {topics.length !==0 && <><h3>Topics Selected:</h3><ul>{topicList}</ul></> }
             </form>
-            {topics.length > 0 && <form action={submitQuiz}>
-                <h4>Select the total number of questions you want</h4>
+            {topics.length > 0 &&
+            <form action={submitQuiz} id="questionForm">
+                <label>Total number of questions</label>
                 <select name="numQuestions" defaultValue="" onChange={(e) => {setMultiChoice(e.target.value)}}>
                     <option value="" disabled>--Select the # of questions--</option>
                     <option value={5}>5</option>
                     <option value={10}>10</option>
                     <option value={20}>20</option>
                 </select>
-                <h4>Select the number of multiple choice questions</h4>
-                <section>
-                    <p>0</p>
-                    <input type="range" name="numMultipleChoice" max={multiChoice} min={0} step={1} defaultValue={Math.floor(multiChoice / 2)}/>
-                    <p>{multiChoice}</p>
-                </section>
+                <label htmlFor="mutliChoice">Number of multiple choice questions</label>
+                <input id="multiChoice" type="number" defaultValue={5} max={multiChoice} min={0} step={1} />
                 <button>Generate Quiz</button>
             </form>}
-        </>
+        </main>
     )
 }
 
