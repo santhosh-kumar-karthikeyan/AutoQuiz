66430fb874fa2eaefdcdf0c4f95dd2bc446f112d
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-14 17:54:20 +0530

    fix logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
-import quizLogo from "/autoQuizLogo.png";
 import "./Header.css";
 function Header() {
     return (
         <header>
-            <img src={quizLogo} alt="AutoQuiz logo" />
+            <img src= "/autoQuizLogo.png" alt="AutoQuiz logo" />
             <h1>Auto Quiz</h1>
         </header>
     )
