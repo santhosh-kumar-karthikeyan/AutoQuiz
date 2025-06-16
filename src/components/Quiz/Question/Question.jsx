ae9c3782d79d0c399485225b4020d45f26724a32
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-16 22:32:30 +0530

    add logic for answer validation

--- src/components/Quiz/Question/Question.jsx
+++ src/components/Quiz/Question/Question.jsx
@@ -5,13 +5,16 @@ function Question(props) {
     const optionList = question.options.map(option => {
         return (
         <li>
-                {question.type === "mcq" ? <input type="radio" /> : <input type="checkbox" />} {option} { question.type}
+                {question.type === "mcq" ?
+                    <input type="radio" /> :
+                    <input type="checkbox" />
+                } {option} 
         </li>
         )
     });
     return (
         <main>
-            <small>{question.qId}</small> <p>{question.question}</p>
+            <small>{question.qId + 1}</small> <p>{question.question}</p>
             <ul>
                 {optionList}
             </ul>
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,13 +1,32 @@
 import "./Quiz.css"
 import Question from "./Question/Question";
+import { useState } from "react";
 
 function Quiz(props) {
     const quiz = props.quiz;
-    const questions = quiz.map(question => <li><Question question={question} /></li>);
+    const questions = quiz.map(question => <li><Question question={question} setAnswers={setAnswers}/></li>);
+    const [answers, setAnswers] = useState([[]]);
+    const scorePerQuestion = 100 / quiz.length;
+    const [score, setScore] = useState(-1);
+    function validateAnswers() {
+        let totalScore = 0;
+        for (let question in quiz) {
+            if (question.type === "mcq" && question.answerIndex === answers[question.qId])
+                totalScore += scorePerQuestion;
+            if (question.type === "msq" && question.answerIndex.sort() === answers[question.qId].sort())
+                totalScore += scorePerQuestion;
+        }
+        setScore(totalScore);
+    }
     return (
-    <ul>
+    <form action={validateAnswers}>
         {questions}
-    </ul>)
+            {
+            score < 0 ?
+                <button>Submit Answers</button> :
+                <h4>Your score is: {score}</h4>
+            }
+    </form >)
 }
 
 export default Quiz;
