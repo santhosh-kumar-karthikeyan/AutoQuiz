f90456007749764801d7e731b8606a91edcd647e
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-21 17:54:48 +0530

    fix analytics with the correct import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
